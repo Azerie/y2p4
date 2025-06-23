14aa45538a5b5fbc8baa0458b37bb0eb336a5d40
Azzy <151638970+Azerie@users.noreply.github.com>
2025-06-23 11:40:14 +0200

    restructured voicelines and subtitles a bit

--- Assets/Scripts/Interactables/ScriptableVoiceline.cs
+++ Assets/Scripts/Interactables/ScriptableVoiceline.cs
@@ -1,16 +1,17 @@
 using System;
 using UnityEngine;
+using FMODUnity;
 
 [CreateAssetMenu(fileName = "NewVoiceline", menuName = "Voiceline")]
 public class Voiceline : ScriptableObject
 {
     [Header("Basic Info")]
-    [SerializeField] private AudioClip audio;
+    [SerializeField] private EventReference audio;
     [SerializeField] private string subtitlesText;
-    [SerializeField] private float subtitlesTime;
+    [SerializeField] private float subtitlesTime = 3f;
 
     // Properties for access
-    public AudioClip Audio => audio;
+    public EventReference Audio => audio;
     public string SubtitlesText => subtitlesText;
     public float SubtitlesTime => subtitlesTime;
 }
