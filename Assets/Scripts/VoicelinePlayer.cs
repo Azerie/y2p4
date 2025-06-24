3a73fee229d5ba5fe46efc510b5d3e481bf497e0
Azzy <151638970+Azerie@users.noreply.github.com>
2025-06-24 11:06:28 +0200

    subtitles/voicelines for interactable objects

--- Assets/Scripts/VoicelinePlayer.cs
+++ Assets/Scripts/VoicelinePlayer.cs
@@ -7,13 +7,16 @@ public class VoicelinePlayer : MonoBehaviour
 {
     public void PlayVoiceline(Voiceline line)
     {
-        if (gameObject.CompareTag("Player"))
-        {
-            RuntimeManager.PlayOneShot(line.Audio);
-        }
-        else
-        {
-            RuntimeManager.PlayOneShot(line.Audio, transform.position);
+        if (!line.Audio.IsNull)
+        {   
+            if(gameObject.CompareTag("Player"))
+            {
+                RuntimeManager.PlayOneShot(line.Audio);
+            }
+            else
+            {
+                RuntimeManager.PlayOneShot(line.Audio, transform.position);
+            }
         }
         SubtitlesManager.SetSubtitles(line);
     }
