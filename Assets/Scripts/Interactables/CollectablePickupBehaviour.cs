5ea91c02947b58515bdf474cd625fa3188d62612
George Didenko <jed91991199@gmail.com>
2025-06-26 14:18:36 +0200

    Trying clock not cock

--- Assets/Scripts/Interactables/CollectablePickupBehaviour.cs
+++ Assets/Scripts/Interactables/CollectablePickupBehaviour.cs
@@ -9,7 +9,7 @@ public class CollectablePickupBehaviour : InteractableBehaviourAbstract
     public override void OnInteract()
     {
         base.OnInteract();
-        EvidenceManager.GetInstance().AddLine(collectableInfo.JournalEntry);
+        EvidenceManager.GetInstance().AddEntry(collectableInfo);
         Destroy(gameObject);
     }
 }
