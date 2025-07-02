d419a020edff1c3544ac9fb367a66ef3aefeb17c
koolaitman <kaytelintelo@icloud.com>
2025-07-02 12:12:45 +0200

    megre

--- Assets/keyAppearScript.cs
+++ Assets/keyAppearScript.cs
@@ -7,10 +7,15 @@ public class keyAppearScript : MonoBehaviour
 {
     public GameObject lastKey;
     public GameObject keyAppear;
+    public string triggeringTag = "Player";
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        lastKey.transform.position = new Vector3 (keyAppear.transform.position.x, keyAppear.transform.position.y , keyAppear.transform.position.z);
+        if (other.transform.parent != null && other.transform.parent.CompareTag(triggeringTag))
+        {
+
+            lastKey.transform.position = new Vector3(keyAppear.transform.position.x, keyAppear.transform.position.y, keyAppear.transform.position.z);
+        }
     }
 }
 
