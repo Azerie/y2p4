a6deed53cf7ebe389b3d91bcea052118cb1b12cf
Azzy <151638970+Azerie@users.noreply.github.com>
2025-07-04 12:45:51 +0200

    candles puzzle visual + animation trigger

---
+++ Assets/Scripts/AnimationZone.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationZone : MonoBehaviour
+{
+    [SerializeField] private Animator _aniamtor;
+    [SerializeField] private string animationName;
+    [SerializeField] private Voiceline voicelineToPlay;
+    private bool isFirst = true;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent != null && other.transform.parent.CompareTag("Player") && !other.isTrigger)
+        {
+            if(isFirst) {
+                _aniamtor.Play(animationName);
+                other.transform.parent.GetComponent<VoicelinePlayer>().PlayVoiceline(voicelineToPlay);
+                isFirst = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Interactables/CandlePuzzleEndBehaviour.cs
+++ Assets/Scripts/Interactables/CandlePuzzleEndBehaviour.cs
@@ -6,6 +6,8 @@ using UnityEngine.Events;
 public class CandlePuzzleEndBehaviour : MonoBehaviour
 {
     [SerializeField] int requiredCandlesNumber;
+    [SerializeField] List<GameObject> gameObjectsToDisable;
+    [SerializeField] List<GameObject> gameObjectsToEnable;
     int currentCandlesNumber = 0;
     PuzzleItemReveal itemReveal;
 
@@ -13,11 +15,21 @@ public class CandlePuzzleEndBehaviour : MonoBehaviour
     {
         CandlePuzzlePieceBehaviour.CandleLit += AddCandle;
         itemReveal = GetComponent<PuzzleItemReveal>();
+        if (gameObjectsToDisable.Count != requiredCandlesNumber || gameObjectsToEnable.Count != requiredCandlesNumber)
+        {
+            Debug.LogWarning("wrong amount of gameobjects to disable/enable assigned");
+        }
     }
 
     void AddCandle() {
+        if (gameObjectsToDisable.Count == requiredCandlesNumber || gameObjectsToEnable.Count == requiredCandlesNumber)
+        {
+            gameObjectsToDisable[currentCandlesNumber].SetActive(false);
+            gameObjectsToEnable[currentCandlesNumber].SetActive(true);
+        }
         currentCandlesNumber += 1;
-        if(currentCandlesNumber >= requiredCandlesNumber) {
+        if (currentCandlesNumber >= requiredCandlesNumber)
+        {
             itemReveal.EndPuzzle();
         }
     }
